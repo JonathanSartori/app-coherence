910579d01adc6a6ae7d144ac3606d50bf6d8787d
Tiberian <tiberian@free.fr>
2026-02-21 22:54:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,14 @@
-const CACHE_NAME = 'platypus-flow-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon-512.png'
-];
+const CACHE_NAME = 'platypus-flow-v3';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
 
-// Nettoyage des anciens caches lors de la mise à jour
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
 });
