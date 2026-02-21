c7c3cf7c1213667ed95ab66b126ba83ab1c06d52
Tiberian <tiberian@free.fr>
2026-02-21 22:26:00 +0100

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,21 +1,34 @@
-const CACHE_NAME = 'respizen-v1';
+const CACHE_NAME = 'platypus-flow-v2';
 const ASSETS = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-512.png'
 ];
 
-// Installation : Mise en cache des fichiers
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Stratégie : Répondre avec le cache, sinon le réseau
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+// Nettoyage des anciens caches lors de la mise à jour
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
