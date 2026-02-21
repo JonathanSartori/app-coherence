a9a8e8a8e9a28eda26a11dbd1535a698f9f83ab5
Tiberian <tiberian@free.fr>
2026-02-21 23:20:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = 'platypus-flow-v3';
-const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json', './icon-512.png'];
+const CACHE_NAME = 'platypus-flow-v4'; // C'est cette ligne qui force la mise à jour
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-512.png'
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
 
 self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((k) => k !== CACHE_NAME && caches.delete(k))
+    ))
+  );
 });
